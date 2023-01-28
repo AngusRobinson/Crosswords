aaf0dd0aa684b73ac109125aa400cf2d625ebec0
SkullFrog7960 <agmrobinson@icloud.com>
2023-01-28 17:24:48 +0000

    Added rollbysetter to analysis

--- Quiptic_analysis.r
+++ Quiptic_analysis.r
@@ -1,6 +1,7 @@
 library(tidyr)
 library(dplyr)
 library(zoo)
+library(ggplot2)
 
 quiptics <- read.csv('~/Documents/Activity/Quiptic_tracker.csv')
 View(quiptics)
@@ -15,3 +16,13 @@ quiptics$rollingavge <- rollmean(quiptics$Mins_taken, k=10,fill=NA, align='right
 bysetter <- quiptics%>%
   group_by(Setter)%>%
   summarise_at(vars(Mistakes,Mins_taken), list(name=mean))
+
+#Add a rolling average by setter column
+quiptics$rollbysetter <- NA
+for (x in bysetter$Setter) {
+  quiptics[quiptics$Setter==x,]$rollbysetter <- rollmean(quiptics[quiptics$Setter==x,]$Mins_taken,k=5,fill=NA,align='right')}
+
+#Plot over time
+ggplot(data=quiptics)+
+  geom_path(mapping=aes(x=Quiptic_number,y=rollbysetter,color=Setter))+
+  geom_path(mapping=aes(x=Quiptic_number,y=rollingavge),color='blue')
